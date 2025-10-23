4bb2004fe7b09ccb14213b52c038540a8e267a3d
Soy Daniel <do7269301@gmail.com>
2025-10-22 20:54:38 -0500

    adelanto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
-"use client"
+"use client";
 import { Button } from "@/components/button/Button";
 
-
 export default function Home() {
-
   const handleClick = () => {
-    console.log("se hizo click")
-  }
+    console.log("se hizo click");
+  };
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>Hola mundo</div>
-      <Button onClick={handleClick} label="Click" />
-    </div>
+    <>
+      <div className="navBar">
+        <div>
+          <div className="P">Portfolio</div>
+        </div>
+        <div className="navigate">
+          <div className="opciones"><button onClick={handleClick}>Inicio</button></div>
+          <div className="opciones"><button onClick={handleClick}>Sobre Mí</button></div>
+          <div className="opciones"><button onClick={handleClick}>Proyectos</button></div>
+          <div className="opciones"><button onClick={handleClick}>Servicios</button></div>
+          <div className="opciones"><button onClick={handleClick}>Contacto</button></div>
+        </div>
+      </div>
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 hero">
+        
+        <div className="contenido">
+          <h1>
+            <span className="block span1">DESARROLLADOR </span>
+            <span className="block span2">WEB</span>
+          </h1>
+          <div>
+            <p>
+              Este es mi portafolio, Transformo ideas audaces en experiencias
+              digitales únicas. Diseñador, desarrollador y aventurero que crea
+              con pasión y sin límites.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
+
+//<Button onClick={handleClick} label="Click" />
