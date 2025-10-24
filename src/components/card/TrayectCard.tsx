a5b3def8e7a169489e9fa13a348a5786f01e4e50
Daniels0809 <do7269301@gmail.com>
2025-10-24 16:57:49 -0500

    xd

--- src/components/card/TrayectCard.tsx
+++ src/components/card/TrayectCard.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
 import style from "./trayectCard.module.css"
+import Image from 'next/image';
 
 interface TrayectCardProps {
     age: string;
     title: string;
-    text: string; // Define any props if needed
+    text: string;
+    icon?: React.ReactNode;
 }
 
-export const TrayectCard = ({age, title, text}:TrayectCardProps) => {
+export const TrayectCard = ({age, title, text, img}:TrayectCardProps) => {
 
 
   return (
     <>
+    <div className='flex border-l-1 border-amber-300'>
+      <div className='imagen m-0 pl-0'>
+        <Image
+        alt=''
+        src={img}
+        width={10}
+        height={10}
+        />
+        {img}
+      </div>
         <div className={style.containerMiTrayectoria}>
             <div className={style.age}>{age}</div>
             <div className={style.title}><h2>{title}</h2></div>
             <div className={style.text}>{text}</div>
         </div>
+    </div>
     </>
   )
 }
